60e6bbcb0afa155449fdffcf2236bf0d0fb53b85
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2026-04-25 12:03:23 +0530

    Delete read_pdf.py

--- read_pdf.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-with open("Before.pdf", "rb") as f:
-    reader = PyPDF2.PdfReader(f)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-with open("Before.txt", "w", encoding="utf-8") as out_f:
-    out_f.write(text)
